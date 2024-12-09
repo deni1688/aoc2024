54a936eed7d5e217b89b23c590f68cd675e2cc75
Denis <denis.junuzovic@fleetster.de>
2024-12-09 15:19:31 +0100

    renaming stuff

--- day5/part1.ts
+++ day5/part1.ts
@@ -1,9 +1,11 @@
 import * as fs from 'fs';
 
-let [rawRules, rawReports] = fs.readFileSync('day5/input.txt', 'utf-8').split('\n\n')
+let [rawRules, rawUpdates] = fs.readFileSync('day5/input.txt', 'utf-8').split('\n\n')
 
 const rules= rawRules.split('\n').map(y => y.split('|').map(z => Number(z)))
-const reports = rawReports.split('\n').map(x => x.split(',').map(y => Number(y)))
+const updates = rawUpdates.split('\n').map(x => x.split(',').map(y => Number(y)))
 
 console.log({rules});
-console.log({reports});
+console.log({updates});
+
+
