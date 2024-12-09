fc1fe2eacc68fa1fab2eaaadd61955bc9401a63f
Denis <denis.junuzovic@fleetster.de>
2024-12-09 16:19:24 +0100

    d 5 p 1 done

--- day5/part1.ts
+++ day5/part1.ts
@@ -1,11 +1,31 @@
 import * as fs from 'fs';
 
-let [rawRules, rawUpdates] = fs.readFileSync('day5/input.txt', 'utf-8').split('\n\n')
+let [rawRules, rawUpdates] = fs.readFileSync('day5/input.txt', 'utf-8').trim().split('\n\n')
 
 const rules= rawRules.split('\n').map(y => y.split('|').map(z => Number(z)))
 const updates = rawUpdates.split('\n').map(x => x.split(',').map(y => Number(y)))
 
-console.log({rules});
-console.log({updates});
+let middlePageCount = 0;
+
+for (let row of updates) {
+    let valid = true;
+
+    for (let [left, right] of rules) {
+        let indexOfLeft = row.indexOf(left);
+        let indexOfRight = row.indexOf(right);
+
+        if(indexOfLeft !== -1 && indexOfRight !== -1 && indexOfLeft > indexOfRight) {
+            valid = false;
+            break;
+        }
+    }
+
+    if(valid) {
+        let middleIndex = Math.floor(row.length / 2);
+        middlePageCount += row[middleIndex];
+    }
+}
+
+console.log(middlePageCount);
 
 
