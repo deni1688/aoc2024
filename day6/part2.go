30cffefef2c1c5bc7c3fa0d19b14f10e73404c06
Denis <denis.junuzovic@fleetster.de>
2024-12-16 10:06:04 +0100

    making guard a value object

--- day6/part2.go
+++ day6/part2.go
@@ -13,29 +13,29 @@ var Reset = "\033[0m"
 var Green = "\033[32m"
 
 func main() {
-	grid := newGrid(input, "^")
+	grid := newGrid(input, newGuard("^"))
 	fmt.Println(grid.uniqueVisits())
 }
 
 func (g *Grid) uniqueVisits() int {
 	for {
 		currentPosition := g.findGuard()
-		right := g.getNextGuardTurn()
-		moveDirection := g.getNextGuardMove()
+		right := g.guard.getRightTurn()
+		move := g.guard.getNextMove()
 
 		g.setVisited(currentPosition)
 
-		if g.nextMoveOutOfBound(currentPosition, moveDirection) {
+		if g.nextMoveOutOfBound(currentPosition, move) {
 			break
 		}
 
-		next := g.get(currentPosition, moveDirection)
+		next := g.get(currentPosition, move)
 		if next == "#" {
-			g.set(currentPosition, right)
+			g.set(currentPosition, right.string())
 			g.guard = right
 		} else {
 			g.set(currentPosition, fmt.Sprintf("%s%s%s", Green, "o", Reset))
-			g.set(g.next(currentPosition, moveDirection), g.guard)
+			g.set(g.next(currentPosition, move), g.guard.string())
 		}
 	}
 
@@ -45,10 +45,10 @@ func (g *Grid) uniqueVisits() int {
 type Grid struct {
 	area    [][]string
 	visited map[string]int
-	guard   string
+	guard   *Guard
 }
 
-func newGrid(input string, guard string) *Grid {
+func newGrid(input string, guard *Guard) *Grid {
 	rows := strings.Split(strings.Trim(input, "\n"), "\n")
 	area := make([][]string, len(rows))
 	for i, row := range rows {
@@ -72,71 +72,85 @@ func (g *Grid) string() string {
 	return sb.String()
 }
 
-func (g *Grid) findGuard() coordinate {
+func (g *Grid) findGuard() Position {
 	for i, row := range g.area {
 		for j, cell := range row {
-			if cell == g.guard {
-				return coordinate{i, j}
+			if g.guard.equals(cell) {
+				return Position{i, j}
 			}
 		}
 	}
 
-	return coordinate{-1, -1}
+	return Position{-1, -1}
 }
 
-func (g *Grid) nextMoveOutOfBound(position coordinate, direction coordinate) bool {
+func (g *Grid) nextMoveOutOfBound(position Position, direction Position) bool {
 	rowOutBound := position.x+direction.x < 0 || position.x+direction.x >= len(g.area[0])
 	colOutBound := position.y+direction.y < 0 || position.y+direction.y >= len(g.area)
 
 	return rowOutBound || colOutBound
 }
 
-func (g *Grid) get(position coordinate, direction coordinate) string {
+func (g *Grid) get(position Position, direction Position) string {
 	return g.area[position.y+direction.y][position.x+direction.x]
 }
 
-func (g *Grid) set(position coordinate, value string) {
+func (g *Grid) set(position Position, value string) {
 	g.area[position.y][position.x] = value
 }
 
-func (g *Grid) next(position coordinate, direction coordinate) coordinate {
-	return coordinate{position.y + direction.y, position.x + direction.x}
+func (g *Grid) next(position Position, direction Position) Position {
+	return Position{position.y + direction.y, position.x + direction.x}
 }
 
-func (g *Grid) setVisited(position coordinate) {
+func (g *Grid) setVisited(position Position) {
 	g.visited[position.string()] = g.visited[position.string()] + 1
 }
 
-func (g *Grid) getNextGuardTurn() string {
-	switch g.guard {
+type Guard string
+
+func newGuard(s string) *Guard {
+	return (*Guard)(&s)
+}
+
+func (g *Guard) string() string {
+	return string(*g)
+}
+
+func (g *Guard) getRightTurn() *Guard {
+	switch *g {
 	case "^":
-		return ">"
+		return newGuard(">")
 	case ">":
-		return "v"
+		return newGuard("v")
 	case "v":
-		return "<"
+		return newGuard("<")
 	default:
-		return "^"
+		return newGuard("^")
 	}
 }
 
-func (g *Grid) getNextGuardMove() coordinate {
-	switch g.guard {
+func (g *Guard) getNextMove() Position {
+	switch *g {
 	case "^":
-		return coordinate{-1, 0}
+		return Position{-1, 0}
 	case ">":
-		return coordinate{0, 1}
+		return Position{0, 1}
 	case "v":
-		return coordinate{1, 0}
+		return Position{1, 0}
 	default:
-		return coordinate{0, -1}
+		return Position{0, -1}
 	}
 }
 
-type coordinate struct {
+func (g *Guard) equals(s string) bool {
+	return string(*g) == s
+}
+
+type Position struct {
 	y, x int
 }
 
-func (p coordinate) string() string {
+func (p Position) string() string {
 	return fmt.Sprintf("%d,%d", p.y, p.x)
 }
