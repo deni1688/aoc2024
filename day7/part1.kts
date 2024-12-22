c92c1cde1be0712a412a172e19634a6f0910b997
denis <denisj1688@gmail.com>
2024-12-22 18:03:10 +0100

    first naive approach works for sample but not main input

--- day7/part1.kts
+++ day7/part1.kts
@@ -1,12 +1,86 @@
 import java.io.File
 
-val lines = parse(File("input_sample.txt").readLines())
+val lines = parse(File("input.txt").readLines())
 
-lines.forEach(::println)
+var total: Long = 0
+
+for (i in 0..lines.size-1) {
+    val row = lines[i]
+    val target = row[0]
+
+    var result = check(row, "+");
+
+    if (result == target) {
+        println("Row $i is solved with +")
+        total+=target
+        continue
+    }
+
+    result = check(row, "*");
+
+    if (result == target) {
+        println("Row $i is solved with *")
+        total+=target
+        continue
+    }
+
+    result = check(row, "+*");
+
+    if (result == target) {
+        println("Row $i is solved with +*")
+        total+=target
+        continue
+    }
+
+    result = check(row, "*+");
+
+    if (result == target) {
+        println("Row $i is solved with *+")
+        total+=target
+        continue
+    }
+}
+
+println("Total: $total")
 
 fun parse(input: List<String>) = input.map(String::trim).map {
     it.split(": ")
         .mapIndexed() { index, s -> if (index == 1) s.split(" ") else listOf(s) }
         .flatten()
-        .map(String::toInt)
+        .map(String::toLong)
+}
+
+
+fun check(row: List<Long>, operator: String): Long {
+    var result: Long = 0
+    var nextOp = operator.first().toString()
+    for (j in 1..row.size - 1) {
+        if (j == 1) {
+            result = row[j]
+        } else {
+            when (operator) {
+                "+" -> result += row[j]
+                "*" -> result *= row[j]
+                "+*" -> {
+                    if (nextOp == "+") {
+                        result += row[j]
+                        nextOp = "*"
+                    } else {
+                        result *= row[j]
+                        nextOp = "+"
+                    }
+                }
+                "*+" -> {
+                    if (nextOp == "*") {
+                        result *= row[j]
+                        nextOp = "+"
+                    } else {
+                        result += row[j]
+                        nextOp = "*"
+                    }
+                }
+            }
+        }
+    }
+    return result
 }
